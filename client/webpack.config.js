9f92306b50a3257c644793aa5bb76f84c84c300d
tamekamclean <tamekasmclean@gmail.com>
2022-11-15 14:40:24 -0500

    pluggin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,7 +52,7 @@ module.exports = () => {
               loader: 'babel-loader',
               options: {
                   presets: ['@babel/preset-env'],
-                  plugins: [
+                  pluggins: [
                       '@babel/plugin-proposal-object-rest-spread',
                       '@babel/transform-runtime',
                   ],
