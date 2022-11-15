ee4c351862d3aa6b2943fc8253ec525e92a812b8
tamekamclean <tamekasmclean@gmail.com>
2022-11-15 15:11:22 -0500

    production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'developement',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
